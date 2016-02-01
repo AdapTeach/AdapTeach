7b6207cb61f30090957093b5ba40207831889905
latest <[object Object]>
2016-02-01 16:43:29 +0100

    automate route loading using glob-promise

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const koa = require('koa');
 const logger = require('koa-logger');
+const glob = require('glob-promise');
 
 const app = module.exports = koa(),
   port = process.env.PORT || 8000,
   env = process.env.NODE_ENV || 'development';
 //app.use(logger());
 
-const categoryRouter = require('./router/category');
+glob('./router/*.js')
+  .then((routers) => {
+    routers.forEach((router) => {
+      app.use(require(router).routes());
+    })
+  });
+
+/*const categoryRouter = require('./router/category');
 app.use(categoryRouter.routes());
 
 const itemRouter = require('./router/item');
@@ -16,7 +24,7 @@ const assessmentRouter = require('./router/assessment');
 app.use(assessmentRouter.routes());
 
 const compositeRouter = require('./router/composite');
-app.use(compositeRouter.routes());
+app.use(compositeRouter.routes());*/
 
 app.listen(port);
 console.log('app listening on port:', port, env);
