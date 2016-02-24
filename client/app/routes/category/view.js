e379b9fc050059fa5937a55d39f32808f3b8fb9c
Mikael Couzic <mikaelcouzic@gmail.com>
2016-02-24 12:08:31 +0100

    ViewCategory now works with deep data model

--- client/app/routes/category/view.js
+++ client/app/routes/category/view.js
@@ -8,12 +8,19 @@ const ViewCategory = ({category}) =>
   <div>
     <h1>View Category</h1>
     <h3>
-      {category.parents.slice().reverse().map(parent =>
-        <span key={parent.uuid}><Link to={`/category/${parent.uuid}`}>{parent.name}</Link> > </span>
-      )}
+      <ParentHierarchy category={category}/>
       {category.name}
     </h3>
   </div>
+
+const ParentHierarchy = ({category}) => {
+  const parent = category.parent
+  if (!parent) return <span></span>
+  return <span>
+    <ParentHierarchy category={parent}/>
+    <Link to={`/category/${parent.uuid}`}>{parent.name}</Link> >
+  </span>
+}
 
 const mapStateToProps = (state, props) => ({category: categoryData.get(props.params.id)})
 
