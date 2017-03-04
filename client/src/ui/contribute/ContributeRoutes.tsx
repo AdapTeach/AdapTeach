5ca3f5ecfa9e3c1846ccd12eb7828c6755f9a1ab
Mikael Couzic <mikaelcouzic@gmail.com>
2017-03-04 22:46:46 +0100

    Contribute Composite routes

--- client/src/ui/contribute/ContributeRoutes.tsx
+++ client/src/ui/contribute/ContributeRoutes.tsx
@@ -6,13 +6,17 @@ import {path} from '../../router/path'
 import {DisplayCategory} from './category/display/DisplayCategory'
 import {CreateItem} from './item/create/CreateItem'
 import {DisplayItem} from './item/display/DisplayItem'
+import {CreateComposite} from './composite/create/CreateComposite'
+import {DisplayComposite} from './composite/display/DisplayComposite'
 
 export const ContributeRoutes = () =>
-   <Route path='/contribute' render={() =>    <div>
+   <Route path='/contribute' render={() => <div>
       <ContributeNavigation />
       <Switch>
          <Route exact path={path.contribute.category.create} component={CreateCategory}/>
          <Route exact path={path.contribute.category.display()} component={DisplayCategory}/>
+         <Route exact path={path.contribute.composite.create} component={CreateComposite}/>
+         <Route exact path={path.contribute.composite.display()} component={DisplayComposite}/>
          <Route exact path={path.contribute.item.create} component={CreateItem}/>
          <Route exact path={path.contribute.item.display()} component={DisplayItem}/>
       </Switch>
---
+++ client/src/ui/contribute/composite/display/DisplayComposite.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+
+export const DisplayComposite: React.StatelessComponent<{}> = () =>
+   <h1>DisplayComposite</h1>
